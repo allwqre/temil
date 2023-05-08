707c104347ae2ff529121dbd7f3bc10df2e89213
allwqre <github@mail.allwqre.dev>
2023-05-08 18:44:16 +0200

    feat: interpreter in repl

--- lib/repl.ts
+++ lib/repl.ts
@@ -1,17 +1,22 @@
 import readline from "node:readline/promises";
 import { Lexer } from "./Lexer.js";
 import { Parser } from "./Parser.js";
+import { Interpreter } from "./Interpreter.js";
 
 const inq = readline.createInterface(process.stdin, process.stdout);
 
 for (;;) {
-  const input = await inq.question(">");
+  const input = await inq.question("> ");
 
   try {
     const tokens = new Lexer(input).run();
-    console.log(tokens);
+    console.log("TOKENS:", tokens);
     const ast = new Parser(tokens).run();
-    console.log(ast);
+    console.log("AST:", ast);
+    const result = await new Interpreter(ast, {
+      hello: (exec, ...args) => "world",
+    }).run();
+    console.log("RESULT:", result);
   } catch (e) {
     console.error(e);
   }
