f8d0966612a78b14062b9323cc7f80e3fa08da8f
allwqre <github@mail.allwqre.dev>
2023-05-10 23:10:38 +0000

    feat: export everything

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+export * from "./Temil.js";
+
+export * from "./Lexer.js";
+export * from "./Parser.js";
+export * from "./Interpreter.js";
+
+export * from "./Literal.js";
+export * from "./Operator.js";
+export * from "./OperatorImpl.js";
+export * from "./Expression.js";
+
+export * from "./Token.js";
+
+export { Temil as default } from "./Temil.js";
