41fc4e2e31c2ac03695b36dce414f06615a139eb
allwqre <github@mail.allwqre.dev>
2023-05-11 00:30:33 +0000

    refactor: rename callable

--- lib/Interpreter.ts
+++ lib/Interpreter.ts
@@ -9,7 +9,7 @@ export class Interpreter {
 
   private exec = async (current: Expression): Promise<unknown> => {
     const op_impl = this.lookup[current.op.value];
-    if (!op_impl) throw new Error(`Callable "${current.op.value}" not found.`);
+    if (!op_impl) throw new Error(`Operator "${current.op.value}" not found.`);
     return await op_impl(this.exec, ...current.args);
   };
 
