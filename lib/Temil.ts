e12ebd47cff77be82b4f08129c927f7854d9e080
firleaf <55799811+firleaf@users.noreply.github.com>
2023-07-28 18:24:47 +0200

    fix: expose sync and async runtime in Temil

--- lib/Temil.ts
+++ lib/Temil.ts
@@ -7,7 +7,17 @@ import * as assert from './Assert.js';
 export class Temil {
 	constructor(private readonly lookup: OperatorImplLookup) {}
 
-	public run = async (source: string, context: Context = {}) => {
+	public sync = (source: string, context: Context = {}) => {
+		const lexer = new Lexer(source);
+		const tokens = lexer.run();
+		const parser = new Parser(tokens);
+		const ast = parser.run();
+		const interpreter = new Interpreter(ast, this.lookup, context);
+		const result = interpreter.sync();
+		return result;
+	};
+
+	public async = async (source: string, context: Context = {}) => {
 		const lexer = new Lexer(source);
 		const tokens = lexer.run();
 		const parser = new Parser(tokens);
